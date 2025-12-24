d15449f82d800767e7bed512d3e99aa05b2e19e7
Mitch Hartigan <mitch@dandhwatersystems.com>
2025-12-23 16:12:37 -0800

    feat: combine with section headers

--- src/createHeader.js
+++ src/createHeader.js
@@ -2,12 +2,15 @@
 const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
 const fs = require("fs");
 
-async function createHeaderPdf(title, outputPath) {
+async function createHeaderPdf(sectionTitle, outputPath) {
   const pdfDoc = await PDFDocument.create();
   const page = pdfDoc.addPage([612, 792]); // US Letter size
 
+  const [sectionNumber, ...sectionName] = sectionTitle.split(" ");
+
+  const title = `Section ${sectionNumber}`;
   const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
-  const fontSize = 28;
+  const fontSize = 36;
 
   const textWidth = font.widthOfTextAtSize(title, fontSize);
 
@@ -18,6 +21,18 @@ async function createHeaderPdf(title, outputPath) {
     font,
     color: rgb(0, 0, 0),
   });
+
+
+  const subTitle = sectionName.join(' ');
+  const subTitleWidth = font.widthOfTextAtSize(subTitle, 16)
+
+  page.drawText(subTitle, {
+    x: (612-subTitleWidth)/2,
+    y: (792 / 2) - 30,
+    size: 16,
+    font,
+    color: rgb(0, 0, 0),
+  })
 
   const pdfBytes = await pdfDoc.save();
   fs.writeFileSync(outputPath, pdfBytes);
